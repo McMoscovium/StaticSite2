4f41189519914e2c5ff1ce03a8bbbaeec563319d
Makoto <mc0144832@gmail.com>
2024-10-13 20:21:31 +0900

    pathオブジェクトを使えるように修正

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3001;
-const fs = require("fs");
 
 var htmlData;
 
